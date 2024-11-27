9f184369cdbd61e9c4bb8b70c72aea463dc0623a
Daniel Melancon <dmelancon@gmail.com>
2024-11-26 18:21:26 -0800

    push

--- main/main.cpp
+++ main/main.cpp
@@ -2,6 +2,8 @@
 // CS 314 Group 7 
 // Fall 2024
 
+#include <stdio.h>
+
 
 int main(int argc, char ** argv) {
     return 0;
--- main/records.h
+++ main/records.h
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-#include <string>
+#include <string.h>
 #include <iostream>
 
 // Address string lengths
@@ -17,58 +17,3 @@ const int ADDR_ZIP_LEN = 5;
 // generic user constants
 const int USR_ID_MAX = 999999999; // restrain ID values to 9 digits
 const int USR_NAME_LEN = 25;
-
-
-// Contains the address data for a member or provider
-typedef struct address_struct
-{
-    char house[ADDR_HOUSE_LEN];
-    char city[ADDR_CITY_LEN];
-    char state[ADDR_STATE_LEN];
-    char zip[ADDR_ZIP_LEN];
-} Address;
-
-// Generic user record class.
-class UserRecord
-{
-
-public:
-    UserRecord(/* args */);
-    ~UserRecord();
-
-    unsigned int id; // the user's ID number.
-    char name[USR_NAME_LEN];
-    Address address;
-
-    string string();
-    int write(FILE fd);
-};
-
-class MemberRecord : public UserRecord {
-
-public:
-    MemberRecord(/* args */);
-    ~MemberRecord();
-};
-
-UserRecord::UserRecord(/* args */)
-{
-}
-
-UserRecord::~UserRecord()
-{
-}
-
-// returns a string containing all data members in human-readable format. 
-inline string UserRecord::string()
-{
-    return string();
-}
-
-// Writes the record to a file
-// @param   fd  File designator that the record will be written to
-// @return  Number of bytes written
-inline int UserRecord::write(FILE fd)
-{
-    return 0;
-}
